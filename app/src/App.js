1e83a81e2927dc467227df8128c34888a2602b45
Mrityunjay <mrityun.jay@indegene.com>
2023-01-05 20:37:53 +0530

    Socket.io skeleton setup

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 import Editor from "./Components/Editor";
 
-function App() {
+const App = (props) => {
+  const socket = io("ws://localhost:3001");
+
+  useEffect(() => {
+    socket.emit("message", { message: "Hi" });
+    socket.on("message", (data) => {
+      console.log(data);
+    });
+
+    return () => socket.disconnect();
+  }, [socket]);
+
   return <Editor />;
-}
+};
 
 export default App;

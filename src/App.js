a48b55994d6d52429d648e4b256c5a66af92ff1a
“Mrityunjay <mrityunjay38@gmail.com>
2023-01-05 15:09:09 +0530

    Monaco code editor addition

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Editor from "./Components/Editor";
+
+function App() {
+  return <Editor />;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import CodeClub from "./CodeClub";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <CodeClub />
-  </React.StrictMode>
-);
+root.render(<App />);
